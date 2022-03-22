c9b61be6c6c107482b5b73bb5a0e024695d83923
junseok <njs702@ajou.ac.kr>
2022-03-22 11:08:10 +0900

    시리얼 포트 통신 사용해서 built_in led 제어해보기

--- usbserial/usbserial.ino
+++ usbserial/usbserial.ino
@@ -1,3 +1,5 @@
+#include <string.h>
+
 char character;
 
 // 아두이노 보드가 켜졌을 떄 한 번만 실행되는 함수
@@ -7,12 +9,64 @@ void setup(){
     // 통신 에러 설정 등등
 
     Serial.println("Hello world!"); // 시리얼 모니터에 문자열 출력
+
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void func_basic_led(){
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);                       // wait for a second
+}
+
+// sos 신호 보내는 함수
+void func_sos(){
+    for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+                    delay(200);                       // wait for a second
+                    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+                    delay(200);                       // wait for a second
+                }
+                for(int j=0;j<3;j++){
+                    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+                    delay(1000);                       // wait for a second
+                    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+                    delay(1000);                       // wait for a second
+                }
+                for(int j=0;j<3;j++){
+                    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+                    delay(200);                       // wait for a second
+                    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+                    delay(200);                       // wait for a second
+                }
+            }
 }
 
 // 전원이 공급되는 동안 계속 실행되는 함수
 void loop(){
+    
     if(Serial.available()){ // 시리얼 포트 안에 데이터가 들어온다면
-        character = Serial.read();
-        Serial.print(character);
+        character = Serial.read(); // 통신으로 받은 데이터를 읽어서 저장
+        // Serial.print(character); // 해당 데이터 출력
+
+        if(character == 'a'){
+            Serial.println("Basic LED control, blink 1");
+            func_basic_led();
+        }
+        
+        if(character == 'b'){
+            Serial.println("Basic LED control, repeat 5 times");
+            for(int i=0;i<5;i++){
+                func_basic_led();
+            }
+        }
+
+        if(character == 'c'){
+            Serial.println("Send SOS using LED , repeat 3 times");
+            func_sos();
+        }
+
     }
 }
