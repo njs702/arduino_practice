c668f416a3aff2fd8f7ca85704d0c9ad6f4848b3
junseok <njs702@ajou.ac.kr>
2022-03-23 15:18:35 +0900

    시리얼 통신, LCD 표시 둘다 하기 ver 2

--- lcd1602_I2C_bubble/lcd1602_I2C_bubble.ino
+++ lcd1602_I2C_bubble/lcd1602_I2C_bubble.ino
@@ -1,56 +1,77 @@
-#include <stdlib.h>
-#include <LiquidCrystal_I2C.h>
+#include <limits.h>
 
-#define MAX 500
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-int data[MAX];
 
-void tmp(int *A, int *B)
-{
-    int tmp;
-    tmp = *A;
-    *A = *B;
-    *B = tmp;
+void BubbleSort(uint16_t DataSet[], uint16_t length){
+    for (int i = 0; i < length-1; i++)
+    {
+        for (int j = 0; j < length-(i+1); j++)
+        {
+            if (DataSet[j] > DataSet[j+1])
+            {
+                int temp=DataSet[j+1];
+                DataSet[j+1]=DataSet[j];
+                DataSet[j]=temp;
+            }
+            
+        }
+        
+    }
+    
+}
+void MakeDataSet(uint16_t DataSet[], uint16_t length){
+    for (int i = 0; i < length-1; i++)
+    {
+        DataSet[i]=length-i-1;
+    }
+    
+}
+void MakeRandomSet(uint16_t DataSet[], uint16_t length){
+    for (int i = 0; i < length-1; i++)
+    {
+        DataSet[i]=random(UINT16_MAX);  //UINT16_MAX
+    }
+    
 }
 
+
+
+void PrintArray(uint16_t DataSet[], uint16_t length){
+  
+  for (size_t i = 0; i < 10; i++)
+  {
+    Serial.print(DataSet[i]);
+    Serial.print(" ");
+  }
+  for (size_t i = 0; i < 10; i++)
+  {
+    Serial.print(DataSet[length-11+i]);
+    Serial.print(" ");
+  }
+  Serial.println("...");
+}
 void setup()
 {
-    
+
     Serial.begin(9600);
-    
-    for(int i = 0 ; i < MAX ; i++)
-        data[i] = rand();
-    Serial.print("before \n");
-    Serial.println("up");
-    for(int i = 0; i<10 ; i++)Serial.println(data[i]);
-    Serial.println("down");
-    for(int i = 0; i<10 ; i++)Serial.println(data[MAX - 9 + i]);
-    for(int j =0;j<MAX ; j++)
-        for(int i = 0; i < MAX-1 ; i++)
-        {
-            if(data[i]<data[i+1])
-            {
-                tmp(&data[i], &data[i+1]);
-            }
-        }
-    Serial.print("after \n");
-    Serial.println("up");
-    for(int i = 0; i<10 ; i++)Serial.println(data[i]);
-    Serial.println("down");
-    for(int i = 0; i<10 ; i++)Serial.println(data[MAX - 9 + i]);
-    lcd.init();  
-    lcd.backlight();
+    randomSeed(analogRead(0));
+
 }
 void loop()
 {
-    //if(Serial.available())
+    uint16_t _DataSet[800]={6,4,2,3,1,5};
+    uint16_t length=sizeof(_DataSet)/sizeof(_DataSet[0]);
+    uint16_t i=0;
     
-        for(int i = 0; i<10 ; i++){
-            lcd.setCursor(0, 0); 
-            lcd.print(data[i]);
-            lcd.setCursor(0, 1); 
-            lcd.print(data[MAX - 9 + i]);
-            delay(1000);
-        }
-    
+    Serial.print( "Before : ");
+    MakeRandomSet(_DataSet,length);
+    PrintArray(_DataSet,length);
+    for (size_t i = 0; i < 100; i++)
+    {
+        //MakeDataSet(_DataSet,length);
+        MakeRandomSet(_DataSet,length);
+        BubbleSort(_DataSet,length);
+    }
+    Serial.print( "After : ");
+    PrintArray(_DataSet,length);
+
 }
