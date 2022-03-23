96886d1cfe54dfcfa4df9f2f8c1acf66c206fca0
junseok <njs702@ajou.ac.kr>
2022-03-23 12:05:14 +0900

    lcd 16x2 사용해서 현재시각 출력해보기

---
+++ lcd1602_I2C_curTime/lcd1602_I2C_curTime.ino
@@ -0,0 +1,51 @@
+//#include <LiquidCrystal_I2C_Hangul.h>
+
+#include <core_build_options.h>
+#include <swRTC.h>
+#include <stdlib.h>
+
+#include <LiquidCrystal_I2C.h>
+
+//#include <core_build_options.h>
+//#include <swRTC.h>
+
+swRTC rtc; //클래스 개체 선언
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup(){
+    lcd.init(); // lcd 초기화
+    lcd.backlight();
+    rtc.stopRTC();
+    rtc.setTime(12,0,0);
+    rtc.setDate(23,3,2022);
+    rtc.startRTC();
+}
+
+void loop(){
+    
+    int a = 4;
+    
+    lcd.setCursor(0,0);
+    //itoa(rtc.getYear(),year,5);
+    lcd.print(rtc.getYear());
+    lcd.setCursor(a,0);
+    lcd.print('/');
+    if(rtc.getMonth()<10){
+        lcd.setCursor(a+1,0);
+        lcd.print(rtc.getMonth());
+        lcd.print('/');
+        lcd.setCursor(a+3,0);
+        lcd.print(rtc.getDay());
+    }
+    else{
+        lcd.setCursor(a+1,0);
+        lcd.print(rtc.getMonth());
+        lcd.print('/');
+        lcd.setCursor(a+4,0);
+        lcd.print(rtc.getDay());
+    }
+    
+    lcd.setCursor(0,1);
+    lcd.print("hello");
+    
+}
